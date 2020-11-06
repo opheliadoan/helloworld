f1c6770d8bbd9c832e461b883c00744a7e60f8f6
anhdoan <anhdoan@uvic.ca>
2020-11-05 22:27:12 -0800

    rabin-karp searching patterns

--- hash_substring.py
+++ hash_substring.py
@@ -1,4 +1,6 @@
 # python3
+import random
+
 
 def read_input():
     return (input().rstrip(), input().rstrip())
@@ -8,12 +10,25 @@ def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 
+def PolyHash(S, p, x):
+    hash = 0
+    for i in reversed(S):
+        hash = (hash * x + ord(i)) % p
+    return hash
+
+
 def get_occurrences(pattern, text):
-    return [
-        i
-        for i in range(len(text) - len(pattern) + 1)
-        if text[i:i + len(pattern)] == pattern
-    ]
+    _prime = 1000000007
+    x = random.randint(1, _prime - 1)
+    result = []
+    pHash = PolyHash(pattern, _prime, x)
+    for i in range(len(text) - len(pattern) + 1):
+        tHash = PolyHash(text[i: i + len(pattern)], _prime, x)
+        if pHash != tHash:
+            continue
+        if text[i:i + len(pattern)] == pattern:
+            result.append(i)
+    return result
 
 
 if __name__ == '__main__':
