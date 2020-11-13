ea053a5299b8c90b6801e65fc9a14afd9f923361
anhdoan <anhdoan@uvic.ca>
2020-11-12 20:59:23 -0800

    initialization

--- hash_substring.py
+++ hash_substring.py
@@ -13,8 +13,23 @@ def print_occurrences(output):
 def PolyHash(S, p, x):
     hash = 0
     for i in reversed(S):
-        hash = (hash * x + ord(i)) % p
+        hash = ((hash * x + ord(i)) % p + p) % p
     return hash
+
+
+def precompute_hash(pattern, text, p, x):
+    result = [None] * (len(text) - len(pattern) + 1)
+    S = text[(len(text) - len(pattern)):len(text)]
+    result[len(text) - len(pattern)] = PolyHash(S, p, x)
+
+    y = 1
+    for i in range(1, len(pattern) + 1):
+        y = (y * x) % p
+    for i in range(len(text) - len(pattern) - 1, -1, -1):
+        result[i] = (x * result[i+1] + ord(text[i]) -
+                     y * ord(text[i + len(pattern)])) % p
+
+    return result
 
 
 def get_occurrences(pattern, text):
@@ -22,9 +37,10 @@ def get_occurrences(pattern, text):
     x = random.randint(1, _prime - 1)
     result = []
     pHash = PolyHash(pattern, _prime, x)
+    hashes = precompute_hash(pattern, text, _prime, x)
     for i in range(len(text) - len(pattern) + 1):
-        tHash = PolyHash(text[i: i + len(pattern)], _prime, x)
-        if pHash != tHash:
+
+        if pHash != hashes[i]:
             continue
         if text[i:i + len(pattern)] == pattern:
             result.append(i)
