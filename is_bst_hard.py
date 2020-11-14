6faf5b08988e051604463d4e6be6192afc1e4e79
anhdoan <anhdoan@uvic.ca>
2020-11-14 00:21:00 -0800

    bst with duplicate

--- is_bst_hard.py
+++ is_bst_hard.py
@@ -3,13 +3,29 @@
 import sys
 import threading
 
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(2*10**9)  # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
+
+
+def IsBinarySearchTreeHelper(tree, i):
+    left = tree[i][1]
+    right = tree[i][2]
+    if left != -1:
+        if tree[left][0] >= tree[i][0]:
+            return False
+        return IsBinarySearchTreeHelper(tree, left)
+    if right != -1:
+        if tree[right][0] < tree[i][0]:
+            return False
+        return IsBinarySearchTreeHelper(tree, right)
+    return True
 
 
 def IsBinarySearchTree(tree):
     # Implement correct algorithm here
-    return True
+    if len(tree) == 0:
+        return True
+    return IsBinarySearchTreeHelper(tree, 0)
 
 
 def main():
