c3f4de7114e1483dfb745e4df3c26a4029b48828
anhdoan <anhdoan@uvic.ca>
2020-11-13 22:20:51 -0800

    check if bst logic added

--- is_bst.py
+++ is_bst.py
@@ -7,9 +7,21 @@ sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**25)  # new thread will get stack of such size
 
 
+def IsBinarySearchTreeHelper(tree, result, i):
+    left = tree[i][1]
+    right = tree[i][2]
+    if left != -1:
+        IsBinarySearchTreeHelper(tree, result, left)
+    result.append(tree[i][0])
+    if right != -1:
+        IsBinarySearchTreeHelper(tree, result, right)
+
+
 def IsBinarySearchTree(tree):
     # Implement correct algorithm here
-    return True
+    inorder = []
+    IsBinarySearchTreeHelper(tree, inorder, 0)
+    return (inorder == sorted(inorder))
 
 
 def main():
