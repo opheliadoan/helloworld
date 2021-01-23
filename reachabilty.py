d4b8646aa254743202759237890ce800b6c0a3c2
anhdoan <anhdoan@uvic.ca>
2021-01-22 23:45:39 -0800

    recursion

--- reachabilty.py
+++ reachabilty.py
@@ -2,10 +2,19 @@
 
 import sys
 
+reachability = 0
 
-def reach(adj, x, y):
+
+def reach(adj, visited, x, y):
     # write your code here
-    return 0
+    global reachability
+    visited[x] = True
+    if y in adj[x]:
+        reachability = 1
+    for t in adj[x]:
+        if not visited[t]:
+            reach(adj, visited, t, y)
+    return reachability
 
 
 if __name__ == '__main__':
@@ -16,8 +25,9 @@ if __name__ == '__main__':
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     x, y = data[2 * m:]
     adj = [[] for _ in range(n)]
+    visited = [False for _ in range(n)]
     x, y = x - 1, y - 1
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+    print(reach(adj, visited, x, y))
