ca6bc92faa4342e706ebe1fb9d1f66ebeb9d86f9
anhdoan <anhdoan@uvic.ca>
2021-01-31 22:39:08 -0800

    detect cycle with dfs

--- acyclicity.py
+++ acyclicity.py
@@ -3,8 +3,28 @@
 import sys
 
 
-def acyclic(adj):
-    return 0
+def dfs(v, adj, visited, onStack):
+    visited[v] = True
+    onStack[v] = True
+    for i in adj[v]:
+        if not visited[i]:
+            return dfs(i, adj, visited, onStack)
+        elif (onStack[i]):
+            return True
+    onStack[v] = False
+    return False
+
+
+def acyclic(adj, n):
+    result = 0
+    visited = [False for _ in range(n)]
+    onStack = [False for _ in range(n)]
+    edgeTo = [0 for _ in range(n)]
+    for i in range(n):
+        if not visited[i] and result == 0:
+            if dfs(i, adj, visited, onStack):
+                result = 1
+    return result
 
 
 if __name__ == '__main__':
@@ -15,5 +35,5 @@ if __name__ == '__main__':
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-    print(acyclic(adj))
+        adj[b - 1].append(a - 1)
+    print(acyclic(adj, n))
