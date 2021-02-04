a024a165322a1362ec6f29a4bbd9115411f53eca
anhdoan <anhdoan@uvic.ca>
2021-02-03 22:58:59 -0800

    message: strongly connected components

--- strongly_connected.py
+++ strongly_connected.py
@@ -2,12 +2,54 @@
 
 import sys
 
+counter = 1
+
 sys.setrecursionlimit(200000)
 
 
-def number_of_strongly_connected_components(adj):
+def postorder(adj, pre, post, marked, v):
+    global counter
+    marked[v] = 1
+    pre[v] = counter
+    counter += 1
+    for i in adj[v]:
+        if not marked[i]:
+            postorder(adj, pre, post, marked, i)
+    post[v] = counter
+    counter += 1
+
+
+def reversed_graph(edges):
+    reversed_adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        reversed_adj[b - 1].append(a - 1)
+    return reversed_adj
+
+
+def explore(adj, visited, v):
+    visited[v] = 1
+    for i in adj[v]:
+        if not visited[i]:
+            explore(adj, visited, i)
+
+
+def number_of_strongly_connected_components(adj, n, edges):
     result = 0
     # write your code here
+    pre = [0 for _ in range(n)]
+    post = [0 for _ in range(n)]
+    marked = [0 for _ in range(n)]
+    visited = [0 for _ in range(n)]
+    reversed_adj = reversed_graph(edges)
+    for i in range(n):
+        if not marked[i]:
+            postorder(reversed_adj, pre, post, marked, i)
+    desc_post = sorted(post, reverse=True)
+    for p in desc_post:
+        ind = post.index(p)
+        if not visited[ind]:
+            explore(adj, visited, ind)
+            result += 1
     return result
 
 
@@ -20,4 +62,4 @@ if __name__ == '__main__':
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
-    print(number_of_strongly_connected_components(adj))
+    print(number_of_strongly_connected_components(adj, n, edges))
