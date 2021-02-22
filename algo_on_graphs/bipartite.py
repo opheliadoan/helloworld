f1b84b1cc247e587cddeb8c77dfdb807c4d96643
anhdoan <anhdoan@uvic.ca>
2021-02-21 21:25:03 -0800

    use bfs to color graph and check adj

--- algo_on_graphs/bipartite.py
+++ algo_on_graphs/bipartite.py
@@ -4,9 +4,27 @@ import sys
 import queue
 
 
-def bipartite(adj):
+def color(adj):
+    coloredGraph = [-1 for _ in range(len(adj))]
+    coloredGraph[0] = 0
+    Q = [0]
+    while (len(Q) > 0):
+        u = Q.pop(0)
+        for v in adj[u]:
+            if coloredGraph[v] == -1:
+                coloredGraph[v] = 1 if coloredGraph[u] == 0 else 0
+                Q.append(v)
+    return coloredGraph
+
+
+def bipartite(adj, edges):
     # write your code here
-    return -1
+    # 0 for white, 1 for black
+    coloredGraph = color(adj)
+    for (a, b) in edges:
+        if coloredGraph[a - 1] == coloredGraph[b - 1]:
+            return 0
+    return 1
 
 
 if __name__ == '__main__':
@@ -19,4 +37,4 @@ if __name__ == '__main__':
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(bipartite(adj))
+    print(bipartite(adj, edges))
