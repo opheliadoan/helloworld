88b78b13c6091fed4a30ee19de30bc80d52653f7
anhdoan <anhdoan@uvic.ca>
2021-03-11 22:38:30 -0800

    use dijkstra to compute min cost

--- algo_on_graphs/dijkstra.py
+++ algo_on_graphs/dijkstra.py
@@ -4,9 +4,31 @@ import sys
 import queue
 
 
+def extractMin(H):
+    min_pair = H.get()
+    return min_pair[1]
+
+
 def distance(adj, cost, s, t):
     # write your code here
-    return -1
+    result = -1
+    dist = [(sys.maxsize * 2 + 1) for _ in range(len(adj))]
+    prev = [-1 for _ in range(len(adj))]
+    dist[s] = 0
+    H = queue.PriorityQueue(len(adj))
+    for i in range(len(adj)):
+        H.put((dist[i], i))
+    while not H.empty():
+        u = extractMin(H)
+        j = 0
+        for v in adj[u]:
+            if dist[v] > dist[u] + cost[u][j]:
+                dist[v] = dist[u] + cost[u][j]
+                prev[v] = u
+            j += 1
+    if dist[t] != sys.maxsize * 2 + 1:
+        result = dist[t]
+    return result
 
 
 if __name__ == '__main__':
@@ -24,3 +46,4 @@ if __name__ == '__main__':
         cost[a - 1].append(w)
     s, t = data[0] - 1, data[1] - 1
     print(distance(adj, cost, s, t))
+    # distance(adj, cost, s, t)
